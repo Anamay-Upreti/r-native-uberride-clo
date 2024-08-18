78cf95d72c81ad8fa29d9c63dc047a225542c4a0
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-18 23:20:52 +0530

    installed essential things

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,10 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-center">Open up App.js to start working on your app!</Text>
-        {/* Correct usage of StatusBar */}
-        <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-      </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className={"text-red-500"}>hello world</Text>
+      <StatusBar style="auto" />
+    
     </SafeAreaView>
   );
 }
