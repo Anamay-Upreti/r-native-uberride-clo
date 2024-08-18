a23e174063ab3b07ccae789403c7ac31a77769d7
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-18 23:38:02 +0530

    started-structuing

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'; // Make sure to import Text from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
