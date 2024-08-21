13071745bc49c7cca3b8e59904b9bed97b483234
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-21 22:45:34 +0530

    adding-compos

---
+++ app/(root)/(tabs)/history.tsx
@@ -0,0 +1,11 @@
+import {View, Text, SafeAreaView} from 'react-native'; // Make sure to import Text from 'react-native'
+
+const History = () => {
+    return (
+        <SafeAreaView>
+            <Text>History</Text>
+        </SafeAreaView>
+    );
+};
+
+export default History;
