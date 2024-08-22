9b035038442f0ac16f491e7ce58a00647b29ed8f
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-22 23:05:22 +0530

    added-basic-structure

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import {View, Text, SafeAreaView} from 'react-native'; // Make sure to import Text from 'react-native'
+
+const Sign-In = () => {
+    return (
+        <SafeAreaView>
+            <Text>Sign-In</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Sign-In;
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import {View, Text, SafeAreaView} from 'react-native'; // Make sure to import Text from 'react-native'
+
+const Sign-Up = () => {
+    return (
+        <SafeAreaView>
+            <Text>Sign-Up</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Sign-Up;
